47a748ad9b7eb3d4312d2f903dfa8d19c182f093
tomhan@rm.dk <tomhan@rm.dk>
2023-02-07 13:17:38 +0100

    Modified 18 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjQ3OTc0/spectral.js?branch_slug=TBH2023&token=61397e03-2f89-4c55-9c35-d17614d5db23";
+export default { extends: ruleset };
